bcafaa0ee89f517b02a77c70bfd5c214a0693ae7
uuronn <uuronprogram@gmail.com>
2024-01-07 22:18:05 +0900

    envの設定

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,18 @@
 import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 import { CatsModule } from './cats/cats.module';
 import { CorsMiddleware } from './middleware/cors.middleware';
 
 @Module({
-  imports: [CatsModule],
+  imports: [
+    ConfigModule.forRoot({
+      // 参照する環境変数として.envを指定する
+      envFilePath: '.env',
+    }),
+    CatsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
