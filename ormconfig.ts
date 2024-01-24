5729cd2704ae606501827a4e1f22443181b55119
uuronn <uuronprogram@gmail.com>
2024-01-25 06:26:40 +0900

    Create: ormconfig.tsを作成env化

---
+++ ormconfig.ts
@@ -0,0 +1,20 @@
+const ormconfig = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT, 10),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  synchronize: true,
+  logging: false,
+  entities: ['src/entity/**/*.ts'],
+  migrations: ['src/migration/**/*.ts'],
+  subscribers: ['src/subscriber/**/*.ts'],
+  cli: {
+    entitiesDir: 'src/entity',
+    migrationsDir: 'src/migration',
+    subscribersDir: 'src/subscriber',
+  },
+};
+
+export default ormconfig;
