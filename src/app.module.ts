833ed5b95b77f239c7a8fe212b8bd1690abc9dfc
uuronn <uuronprogram@gmail.com>
2024-01-25 08:37:06 +0900

    Fix: app.moduleをenv化

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,38 @@
-import { MiddlewareConsumer, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { BlogsModule } from './blogs/blogs.module';
-import { UsersModule } from './users/users.module';
-import { AuthMiddleware } from './middleware/auth.middleware';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+import { User } from './user/user.entity';
+import { UserModule } from './user/user.module';
+import { HelthCheckController } from './helthCheck/helthCheck.controller';
+import { HelthCheckModule } from './helthCheck/helthCheck.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env',
     }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT, 10),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [User],
+      synchronize: true,
+    }),
     BlogsModule,
-    UsersModule,
+    UserModule,
+    HelthCheckModule,
   ],
-  controllers: [AppController],
+  controllers: [AppController, HelthCheckController],
   providers: [AppService],
 })
 export class AppModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer.apply(AuthMiddleware).forRoutes('/users');
-  }
+  configure() {}
+  constructor(private dataSource: DataSource) {}
 }
